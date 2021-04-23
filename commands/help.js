90ee9e40dadedbe5b099065ed4276cb94c730f6f
Cody <multicody10@yahoo.com>
2021-04-22 21:54:35 -0400

    Add files via upload

--- commands/help.js
+++ commands/help.js
@@ -5,6 +5,10 @@ module.exports = {
     dmOnly: false,
     cooldown: 5,
     execute(client, message, args, discord){
-        message.reply("```/ping\nReturns the bot's latency in ms.\n\n/color {color name or #RRGGBB}\nChanges the user's main role color.\n\n/remindme {number}{h/m/s} {message}\nSends a reminder to the user after a specifed time with a message.```");
+        message.reply("```/ping\nReturns the bot's latency in ms."
+        + "\n\n/color {color name or #RRGGBB}\nChanges the user's main role color."
+        + "\n\n/remindme {number}{h/m/s} {message}\nSends a reminder to the user after a specifed time with a message."
+        + "\n\n/roll [max]\nRolls a number from 1-20 unless a max is supplied."
+        + "\n\n/flip\nFlips a coin and returns heads or tails.```");
     }
 }
