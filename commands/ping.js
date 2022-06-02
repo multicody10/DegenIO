09486207654e18ab16260b73f800cbce6f5db910
multicody10 <multicody10@yahoo.com>
2022-06-02 18:18:10 -0400

    Ticket maker modal

--- commands/ping.js
+++ commands/ping.js
@@ -1,53 +1,25 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageActionRow, MessageButton, MessageSelectMenu, Modal } = require('discord.js');
+const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		const selectrow = new MessageActionRow()
-			.addComponents(
-				new MessageSelectMenu()
-					.setCustomId('select')
-					.setPlaceholder('Select Menu')
-					.addOptions([
-						{
-							label: 'Select me',
-							description: 'This is a description',
-							value: 'first_option',
-						},
-						{
-							label: 'You can select me too',
-							description: 'This is also a description',
-							value: 'second_option',
-						},
-					]),
-			);
-
 		const buttonrow = new MessageActionRow()
 			.addComponents(
 				new MessageButton()
-					.setCustomId('button1')
-					.setLabel('Primary')
-					.setStyle('PRIMARY'),
-				new MessageButton()
-					.setCustomId('button2')
-					.setLabel('Secondary')
-					.setStyle('SECONDARY'),
-				new MessageButton()
-					.setCustomId('button3')
-					.setLabel('Success')
+					.setCustomId('open-ticket')
+					.setLabel('Open Ticket')
 					.setStyle('SUCCESS'),
-				new MessageButton()
-					.setCustomId('button4')
-					.setLabel('Danger')
-					.setStyle('DANGER'),
-				new MessageButton()
-					.setURL('http://rabl.tv/ombi/')
-					.setLabel('Link')
-					.setStyle('LINK'),
 			);
-		await interaction.reply({ content: 'Pong!', components: [selectrow, buttonrow] });
+
+		const embed = new MessageEmbed()
+			.setColor('#0099ff')
+			.setTitle('Ticket')
+			.setURL('')
+			.setDescription('Click the button to open a ticket!');
+
+		await interaction.reply({ content: ' ', embeds: [embed], components: [buttonrow] });
 	},
 };
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Intents, MessageActionRow, MessageButton, MessageSelectMenu, Modal } = require('discord.js');
+const { Client, Collection, Intents, MessageActionRow, MessageButton, MessageEmbed, TextInputComponent, Modal } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
@@ -37,11 +37,46 @@ client.on('interactionCreate', async interaction => {
 client.on('interactionCreate', async interaction => {
     if (!interaction.isButton()) return;
 
-    if (interaction.customId === 'button1') {
-        var redRole = interaction.member.guild.roles.cache.find(role => role.name === "Red");
-        interaction.member.roles.add(redRole);
-        await interaction.reply({ content: 'You now have the red role!', ephemeral: true });
+
+    if (interaction.customId === 'open-ticket') {
+        // Create the modal
+        const modal = new Modal()
+            .setCustomId('ticketModal')
+            .setTitle('Start a ticket');
+        // Add components to modal
+        // Create the text input components
+        const ticketName = new TextInputComponent()
+            .setCustomId('ticketNameInput')
+            // The label is the prompt the user sees for this input
+            .setLabel("Subject")
+            // Short means only a single line of text
+            .setStyle('SHORT')
+            .setRequired(true);
+        const ticketMessage = new TextInputComponent()
+            .setCustomId('ticketMessageInput')
+            .setLabel("Message")
+            // Paragraph means multiple lines of text.
+            .setStyle('PARAGRAPH')
+            .setRequired(true);
+        // An action row only holds one text input,
+        // so you need one action row per text input.
+        const firstActionRow = new MessageActionRow().addComponents(ticketName);
+        const secondActionRow = new MessageActionRow().addComponents(ticketMessage);
+        // Add inputs to the modal
+        modal.addComponents(firstActionRow, secondActionRow);
+        // Show the modal to the user
+        await interaction.showModal(modal);
     }
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isModalSubmit()) return;
+    const ticketSubject = interaction.fields.getTextInputValue('ticketNameInput');
+    const ticketMessage = interaction.fields.getTextInputValue('ticketMessageInput');
+    console.log({ ticketSubject, ticketMessage });
+    let channel = interaction.guild.channels.create(ticketSubject, { type: 'GUILD_TEXT' });
+    (await channel).setParent('982042135253618738');
+    await interaction.reply({ content: 'Your ticket has been submitted!', ephemeral: true });
 });
 
 client.login(token);
